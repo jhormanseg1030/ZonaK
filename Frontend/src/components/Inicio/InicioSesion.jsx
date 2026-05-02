cab19be0ce0b092b1c3982b130a41a3ed1a52f32
noniiz <hernandezpoqu@gmail.com>
2026-05-02 11:49:51 -0500

    cambios

---
+++ Frontend/src/components/Inicio/InicioSesion.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { Route, Router } from 'react-router-dom'
+import './InicioSesion.css' 
+
+function InicioSesion(){
+        return(
+            <h1>
+                Hola Mundo
+            </h1>
+    )
+}
+
+export default InicioSesion
